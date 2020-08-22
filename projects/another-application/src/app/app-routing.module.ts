1ec823b2e93189a9741cfc3c6e22819f86d5ced2
helix46 <bryce.abbott@iblong2.com>
2020-08-22 18:21:12 +1000

    init

--- projects/another-application/src/app/app-routing.module.ts
+++ projects/another-application/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'shared-page',
+    loadChildren: () =>
+      import('./shared-lib/shared-lib.module').then((m) => m.SharedLibModule),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./home-page/home-page.module').then((m) => m.HomePageModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
